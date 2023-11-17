b546e88c91d8b1bdeb60f0fb3b4c9d063dfc5a15
nmartinescu <nmartinescu@bitdefender.com>
2023-11-17 08:12:54 +0200

    Error printing

--- AI/Tema4/Network.py
+++ AI/Tema4/Network.py
@@ -1,9 +1,10 @@
 from Layer import Layer
+from Util import Util
 
 
 class Network:
 
-    def __init__(self, input_layer_size, hidden_layer_sizes, output_layer_size, learning_rate=0.1, epochs=1):
+    def __init__(self, input_layer_size, hidden_layer_sizes, output_layer_size, learning_rate=0.1, epochs=2):
         self.layers = []
         self.layers.append(Layer(input_layer_size, hidden_layer_sizes[0]))
         for i in range(1, len(hidden_layer_sizes)):
@@ -24,11 +25,16 @@ class Network:
 
     def evaluate_and_train(self, train_x, train_y):
         for epoch in range(self.epochs):
+            error = 0
             for x, y in zip(train_x, train_y):
                 output = self.evaluate(x)
+                target = [0 for _ in range(self.output_layer_size)]
+                target[y - 1] = 1
+                error += Util.error_function_mse(output, target)
                 print(output)
                 print(y)
                 print()
+            print("Epoch " + str(epoch) + " error: " + str(error / len(train_x)))
 
     def __str__(self):
         layers = ""
